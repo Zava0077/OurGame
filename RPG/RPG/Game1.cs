de674a099674a83961b718ec80772058b9a132cf
RiderRJ <vanyakurpishev@gmail.com>
2022-04-29 18:58:19 +0700

    poludel

--- RPG/RPG/Game1.cs
+++ RPG/RPG/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,18 +10,26 @@ namespace RPG
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D texture;
+        Texture2D exit;
+        Texture2D settings;
+        Texture2D play;
+        int b = 130;
         private readonly Rectangle screenBounds;
         private readonly Matrix screenXform;
-
-
+        Vector2 position = Vector2.Zero;
+        float speed = 5f;
+        MouseState lastMouseState;
+        int offset = 125;
+        int depth = 65;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            var screenScale = graphics.PreferredBackBufferHeight / 1080.0f;
-            screenXform = Matrix.CreateScale(screenScale, screenScale, 1.0f);
+          //  var screenScale = graphics.PreferredBackBufferHeight / 1080.0f;
+           // screenXform = Matrix.CreateScale(screenScale, screenScale, 1.0f);
+
+            IsMouseVisible = true;
         }
 
         protected override void Initialize()
@@ -28,9 +38,11 @@ namespace RPG
         }
 
         protected override void LoadContent()
-        {
+        {       
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            texture = Content.Load<Texture2D>("button");
+            exit = Content.Load<Texture2D>("button");
+            settings = Content.Load<Texture2D>("settings");
+            play = Content.Load<Texture2D>("play");
         }
 
         protected override void UnloadContent()
@@ -42,27 +54,43 @@ namespace RPG
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            Vector3 cursor = new Vector3(Mouse.GetState().X, Mouse.GetState().Y, 0);
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                if (cursor.X > (Window.ClientBounds.Width / 2) - 130 & cursor.X < (Window.ClientBounds.Width / 2) + offset)
+                {
+                    if (cursor.Y > (Window.ClientBounds.Height / 3) - offset & cursor.Y < ((Window.ClientBounds.Height / 3) + (Window.ClientBounds.Height / 3) - (offset + depth)))
+                        ; // Играть
+                    else if (cursor.Y > (Window.ClientBounds.Height / 3) * 2 - offset & cursor.Y < ((Window.ClientBounds.Height / 3) + (Window.ClientBounds.Height / 3) * 2 - (offset + depth)))
+                        ; // Настройки(Мб сложность)
+                    else if (cursor.Y > (Window.ClientBounds.Height) - offset & cursor.Y < ((Window.ClientBounds.Height / 3) + (Window.ClientBounds.Height) - (offset + depth)))
+                        Exit(); //Выход
+                }
+                
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-           // spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, screenXform);
+            // spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, screenXform);
 
 
-            // отрисовка спрайта
-            Vector2 Vector = new Vector2();
-            Vector.X = 100;
-            Vector.Y = 100;
-            Sprite(Vector);
+            // отрисовка спрайта
+            Vector2 Vector10 = new Vector2((Window.ClientBounds.Width / 2) - 130, (Window.ClientBounds.Height / 3)- offset);
+            Vector2 Vector11 = new Vector2((Window.ClientBounds.Width / 2) - 130, (Window.ClientBounds.Height / 3)+ (Window.ClientBounds.Height / 3) - offset);
+            Vector2 Vector12 = new Vector2((Window.ClientBounds.Width / 2) - 130, (Window.ClientBounds.Height / 3)+ ((Window.ClientBounds.Height / 3)*2) - offset);
+            Sprite(Vector10,Vector11,Vector12);
             base.Draw(gameTime);
+
         }
-        protected void Sprite(Vector2 Vector)
+        protected void Sprite(Vector2 Vector, Vector2 Vector1, Vector2 Vector2)
         {
             spriteBatch.Begin();
-            spriteBatch.Draw(texture, Vector, Color.WhiteSmoke);
+            spriteBatch.Draw(play, Vector, Color.WhiteSmoke);
+            spriteBatch.Draw(settings, Vector1, Color.WhiteSmoke);
+            spriteBatch.Draw(exit, Vector2, Color.WhiteSmoke);
             spriteBatch.End();
         }
     }
