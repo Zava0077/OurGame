fc924b1317418a768afc302346c4db9b822a1a48
RiderRJ <vanyakurpishev@gmail.com>
2022-05-01 17:14:34 +0700

    Всё перелопатил, переделал прорисовку меню

--- RPG/RPG/Game1.cs
+++ RPG/RPG/Game1.cs
@@ -4,25 +4,42 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using System.Collections.Generic;
+
 namespace RPG
 {
+
+    enum Stat
+    {
+        MainScreen,
+        Game,
+        Settings,
+        Exit,
+        Final
+    }
+
     public class Game1 : Game
     {
         Menu mn = new Menu();
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         int bittonWidth = 130;
+        int bittonHeight = 100;
         //private readonly Rectangle screenBounds;
         //private readonly Matrix screenXform;
       //  Vector2 position = Vector2.Zero;
       //  float speed = 5f;
         MouseState lastMouseState;
         int offset = 125;
-        //int depth = 65;
-        
+        int depth = 65;
+        Stat status = Stat.MainScreen;
+
+        private List<Component> _gameComponents; //ура
 
         public Game1()
         {
+
+            Button btn = new Button(mn.exit, mn.font);
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             //  var screenScale = graphics.PreferredBackBufferHeight / 1080.0f;
@@ -33,11 +50,12 @@ namespace RPG
             int v3Width = 277; //ширина четвертого спрайта
             int v3Height = 420; //высота четвертого спрайта
 
-            mn.v = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) - offset); // задаем местоположения 3-х кнопок
+            mn.v = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) - offset); // задаем местоположения наших спрайтов
             mn.v1 = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) + (Window.ClientBounds.Height / 3) - offset);
             mn.v2 = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) + ((Window.ClientBounds.Height / 3) * 2) - offset);
             mn.v3 = new Vector2((Window.ClientBounds.Width / 2) - v3Width/2, (Window.ClientBounds.Height / 2) - v3Height/ 2);
-            
+            mn.backv = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) + ((Window.ClientBounds.Height / 3) * 2) - offset); //кнопка назад
+
             mn.color = Color.WhiteSmoke;
             mn.newMenuColor = Color.WhiteSmoke;
 
@@ -50,12 +68,56 @@ namespace RPG
         }
 
         protected override void LoadContent()
-        {       
+        {
+
+
+            var playButton = new Button(mn.play, Content.Load<SpriteFont>("Font"))
+            {
+                Position = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) - offset),
+                Text = "Play",
+            };
+
+            var quitButton = new Button(mn.exit, Content.Load<SpriteFont>("Font"))
+            {
+                Position = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) + ((Window.ClientBounds.Height / 3) * 2) - offset),
+                Text = "Quit",
+            };
+            var settingsButton = new Button(mn.settings, Content.Load<SpriteFont>("Font"))
+            {
+                Position = new Vector2((Window.ClientBounds.Width / 2) - bittonWidth, (Window.ClientBounds.Height / 3) + (Window.ClientBounds.Height / 3) - offset),
+                Text = "Settings",
+            };
+
+            quitButton.Click += QuitButton_Click;
+
+            settingsButton.Click += SettingsButton_click;
+
+            _gameComponents = new List<Component>()
+            {
+                playButton,
+                quitButton,
+                settingsButton,
+            };
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
             mn.exit = Content.Load<Texture2D>("button");
             mn.settings = Content.Load<Texture2D>("settings");
             mn.play = Content.Load<Texture2D>("play");
             mn.test = Content.Load<Texture2D>("робокот");
+            mn.back = Content.Load<Texture2D>("back");
+            mn.font = Content.Load<SpriteFont>("Font");
+        }
+
+
+
+        private void QuitButton_Click(object sender, System.EventArgs e)
+        {
+            Exit();
+        }
+
+        private void SettingsButton_click(object sender, System.EventArgs e)
+        {
+            
         }
 
         protected override void UnloadContent()
@@ -65,13 +127,82 @@ namespace RPG
 
         protected override void Update(GameTime gameTime)
         {
+
+            foreach (var component in _gameComponents)
+                component.Update(gameTime);
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             Vector3 cursor = new Vector3(Mouse.GetState().X, Mouse.GetState().Y, 0);
 
             int w = Window.ClientBounds.Width;
             int h = Window.ClientBounds.Height;
-            mn.ButtonFunction(cursor, w, h, mn.v, mn.v1, mn.v2, mn.v3, spriteBatch, mn.exit, mn.settings, mn.play, mn.test, mn.color, mn.newMenuColor);
+
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                if (cursor.X > (w / 2) - 130 & cursor.X < (w / 2) + offset)
+                {
+                    if (cursor.Y > (h / 3) - offset & cursor.Y < ((h / 3) + (h / 3) - (offset + depth)))// Играть
+                    {
+                        ;
+                    }
+                    if (cursor.Y > (h / 3) * 2 - offset & cursor.Y < ((h / 3) + (h / 3) * 2 - (offset + depth))) // Настройки(Мб сложность)
+                    {
+                        mn.SettingsMenu(mn.v3, spriteBatch, mn.test, mn.newMenuColor, mn.isMenuSettings, false);
+                    }
+                    if (cursor.Y > (h) - offset & cursor.Y < ((h / 3) + (h) - (offset + depth)))
+                    {
+                        if (mn.CurrentStatus == mn.Stats[0] && mn.clickFromMenu == true)
+                        {
+                            Exit();
+                        }
+                        if (cursor.Y > (h) - offset & cursor.Y < ((h / 3) + (h) - (offset + depth)))
+                        {
+                            mn.BackFromSettings(mn.v3, spriteBatch, mn.exit, mn.settings, mn.play, mn.newMenuColor);
+                        }
+                    }
+                }
+            }
+            
+        
+
+            switch (status)
+            {
+                case Stat.MainScreen:
+                    {
+
+                        mn.ButtonFunction(cursor, w, h, mn.v, mn.v1, mn.v2, mn.v3, spriteBatch, mn.exit, mn.settings, mn.play, mn.test, mn.color, mn.newMenuColor);
+                        break;
+                    }
+                case Stat.Game:
+                    {
+                        break;
+                    }
+                case Stat.Settings:
+                    {
+                        mn.isMousePressed = true;
+                        mn.Sprite(mn.v, mn.v1, mn.v2, mn.v3, spriteBatch, mn.exit, mn.settings, mn.play, mn.test, Color.Transparent, mn.newMenuColor, mn.isMousePressed); //стираем меню
+                        mn.isMenuSettings = true;
+                        mn.SettingsMenu(mn.v3, spriteBatch, mn.test, mn.newMenuColor, mn.isMenuSettings, false); //рисуем кота и кнопку
+                        mn.clickFromMenu = false;
+                        break;
+                    }
+                case Stat.Exit:
+                    {
+                        Exit();
+                        break;
+                    }
+                case Stat.Final:
+                    {
+
+                        break;
+                    }
+            }
+
+
+            
+
+          //  mn.ButtonFunction(cursor, w, h, mn.v, mn.v1, mn.v2, mn.v3, spriteBatch, mn.exit, mn.settings, mn.play, mn.test, mn.color, mn.newMenuColor);
             base.Update(gameTime);
         }
 
@@ -79,8 +210,17 @@ namespace RPG
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             // spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, screenXform);
-            mn.Sprite(mn.v, mn.v1, mn.v2,mn.v3, spriteBatch, mn.exit, mn.settings, mn.play,mn.test, mn.color, mn.newMenuColor); // отрисовка спрайта
-            mn.SettingsMenu(mn.v3, spriteBatch, mn.test, mn.newMenuColor, mn.isMenuSettings);
+            // mn.Sprite(mn.v, mn.v1, mn.v2,mn.v3, spriteBatch, mn.exit, mn.settings, mn.play,mn.test, mn.color, mn.newMenuColor, mn.isMousePressed); // отрисовка спрайта
+            //  mn.SettingsMenu(mn.v3, spriteBatch, mn.test, mn.newMenuColor, mn.isMenuSettings, false);
+            spriteBatch.Begin();
+
+            
+
+            foreach (var component in _gameComponents)
+                component.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
--- RPG/RPG/MainMenu/Menu.cs
+++ RPG/RPG/MainMenu/Menu.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -7,15 +6,22 @@ namespace RPG
 {
     public class Menu
     {
+        public string[] Stats = new string[] { "Menu", "Settings", "Play", "Exit" };
+        public string CurrentStatus;
+
         public Texture2D exit;
+        
         public Texture2D settings;
         public Texture2D play;
         public Texture2D test;
+        public Texture2D back;
+        public SpriteFont font;
 
         public Vector2 v;
         public Vector2 v1;
         public Vector2 v2;
         public Vector2 v3;
+        public Vector2 backv;
 
 
         public Color color; //цвет задника кнопок
@@ -23,6 +29,9 @@ namespace RPG
 
         public bool isMousePressed = false;
         public bool isMenuSettings = false;
+        public bool clickFromMenu = true;
+
+        int count = 0;
 
         public Menu() //пустой конструктор класса
         {
@@ -36,38 +45,43 @@ namespace RPG
 
         public void LoadContent()
         {
-          /*  exit = Game1.self.Content.Load<Texture2D>("button");
-            settings = Game1.self.Content.Load<Texture2D>("settings");
-            play = Game1.self.Content.Load<Texture2D>("play");
-            test = Game1.self.Content.Load<Texture2D>("робокот"); */ // потом
+
         }
 
-        public void Sprite(Vector2 v, Vector2 v1, Vector2 v2, Vector2 v3, SpriteBatch spriteBatch, Texture2D exit ,Texture2D settings, Texture2D play,Texture2D test, Color color, Color nMC)
+        public void Sprite(Vector2 v, Vector2 v1, Vector2 v2, Vector2 v3, SpriteBatch spriteBatch, Texture2D exit, Texture2D settings, Texture2D play, Texture2D test, Color color, Color nMC, bool IsMousePressed)
         {
+            CurrentStatus = Stats[0];
             nMC = newMenuColor;
-            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
-                isMousePressed = true;
-            if (isMousePressed == true)
+            if (IsMousePressed == true)
                 color = Color.Transparent;
-            spriteBatch.Begin();
-            spriteBatch.Draw(play, v, color);
-            spriteBatch.Draw(settings, v1, color);
-            spriteBatch.Draw(exit, v2, color);
-            spriteBatch.End();
-        }
-
-        public void SettingsMenu(Vector2 v3, SpriteBatch spriteBatch, Texture2D t, Color nMC, bool isMenuSettings) // Меню настроек
-        {
-            nMC = newMenuColor;
-            if (isMenuSettings == true)
-            {   
+            else
+            {
                 spriteBatch.Begin();
-                spriteBatch.Draw(test, v3, newMenuColor);
+                spriteBatch.Draw(play, v, color);
+                spriteBatch.Draw(settings, v1, color);
+                spriteBatch.Draw(exit, v2, color);
                 spriteBatch.End();
             }
         }
 
-        public void ButtonFunction(Vector3 cursor, int Width, int Height, Vector2 v ,Vector2 v1, Vector2 v2, Vector2 vec3, SpriteBatch spriteBatch, Texture2D e, Texture2D s, Texture2D p, Texture2D test, Color color, Color nMC)
+        public void SettingsMenu(Vector2 v3, SpriteBatch spriteBatch, Texture2D t, Color nMC, bool isMenuSettings, bool QuitProcess) // Меню настроек
+        {
+            nMC = newMenuColor;
+            if (isMenuSettings == true)
+            {
+                CurrentStatus = Stats[1];
+                spriteBatch.Begin();
+                spriteBatch.Draw(test, v3, newMenuColor);
+                spriteBatch.Draw(back, backv, newMenuColor);
+                spriteBatch.End();
+            }
+            else
+                nMC = Color.Transparent;
+            if (QuitProcess == true)
+                Exit();
+        }
+
+        public void ButtonFunction(Vector3 cursor, int Width, int Height, Vector2 v, Vector2 v1, Vector2 v2, Vector2 vec3, SpriteBatch spriteBatch, Texture2D e, Texture2D s, Texture2D p, Texture2D test, Color color, Color nMC)
         {
             int offset = 125; // Смещение по вертикали
             int depth = 65; // Расстояние между кнопками
@@ -76,22 +90,78 @@ namespace RPG
 
             Color nul = Color.Transparent; // пустой цвет
             nMC = newMenuColor;
-            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+
+            if (isMenuSettings == true)
+                CurrentStatus = Stats[1];
+
+            int count = 0;
+
+       /*     if (Mouse.GetState().LeftButton == ButtonState.Pressed)
             {
                 if (cursor.X > (Width / 2) - 130 & cursor.X < (Width / 2) + offset)
                 {
                     if (cursor.Y > (Height / 3) - offset & cursor.Y < ((Height / 3) + (Height / 3) - (offset + depth)))// Играть
-                        Sprite(v, v1, v2,vec3, spriteBatch, e, s, p,test, nul, newMenuColor); 
-                    else if (cursor.Y > (Height / 3) * 2 - offset & cursor.Y < ((Height / 3) + (Height / 3) * 2 - (offset + depth))) // Настройки(Мб сложность)
                     {
-                        Sprite(v, v1, v2,vec3, spriteBatch, e, s, p,test, nul, newMenuColor); //стираем меню
-                        isMenuSettings = true;
-                        SettingsMenu(vec3, spriteBatch, test, newMenuColor, isMenuSettings); //рисуем кота
+                        isMousePressed = true;
+                        Sprite(v, v1, v2, vec3, spriteBatch, e, s, p, test, nul, newMenuColor, isMousePressed); //стираем меню
+                        clickFromMenu = false;
                     }
-                    else if (cursor.Y > (Height) - offset & cursor.Y < ((Height / 3) + (Height) - (offset + depth)))
-                        Exit(); //Выход
+                    if (cursor.Y > (Height / 3) * 2 - offset & cursor.Y < ((Height / 3) + (Height / 3) * 2 - (offset + depth))) // Настройки(Мб сложность)
+                    {
+                        isMousePressed = true;
+                        Sprite(v, v1, v2, v3, spriteBatch, exit, settings, play, test, nul, newMenuColor, isMousePressed); //стираем меню
+                        isMenuSettings = true;
+                        SettingsMenu(v3, spriteBatch, test, newMenuColor, isMenuSettings, false); //рисуем кота и кнопку
+                        clickFromMenu = false;
+                    }
+                    if (cursor.Y > (Height) - offset & cursor.Y < ((Height / 3) + (Height) - (offset + depth)))
+                    {
+                        if (CurrentStatus == Stats[0] && clickFromMenu == true && (count == 0 || count == 3))
+                        {
+                            Exit();
+                        }
+                        if (cursor.Y > (Height) - offset & cursor.Y < ((Height / 3) + (Height) - (offset + depth)))
+                        {
+                            count++;
+                            isMousePressed = false;
+                            SettingsMenu(vec3, spriteBatch, test, newMenuColor, isMenuSettings, false); //стираем кота и кнопку
+                            Sprite(v, v1, v2, vec3, spriteBatch, e, s, p, test, nMC, newMenuColor, isMousePressed); //рисуем меню
+                            isMenuSettings = false;
+                            CurrentStatus = Stats[0];
+                        }
+                    }
                 }
             }
+            */
+            if (CurrentStatus == Stats[3])
+                SettingsMenu(vec3, spriteBatch, test, newMenuColor, isMenuSettings, true);
+        }
+
+        public void Play(Vector2 vec3, SpriteBatch spriteBatch, Texture2D e, Texture2D s, Texture2D p, Color nul)
+        {
+            isMousePressed = true;
+            Sprite(v, v1, v2, vec3, spriteBatch, e, s, p, test, nul, newMenuColor, isMousePressed); //стираем меню
+            clickFromMenu = false;
+        }
+
+        public void Settings(SpriteBatch spriteBatch, Color nul)
+        {
+            isMousePressed = true;
+            Sprite(v, v1, v2, v3, spriteBatch, exit, settings, play, test, nul, newMenuColor, isMousePressed); //стираем меню
+            isMenuSettings = true;
+            SettingsMenu(v3, spriteBatch, test, newMenuColor, isMenuSettings, false); //рисуем кота и кнопку
+            clickFromMenu = false;
+        }
+
+        public void BackFromSettings(Vector2 vec3, SpriteBatch spriteBatch, Texture2D e, Texture2D s, Texture2D p, Color nMC)
+        {
+            isMousePressed = false;
+            SettingsMenu(vec3, spriteBatch, test, newMenuColor, isMenuSettings, false); //стираем кота и кнопку
+            Sprite(v, v1, v2, vec3, spriteBatch, e, s, p, test, nMC, newMenuColor, isMousePressed); //рисуем меню
+            isMenuSettings = false;
+            CurrentStatus = Stats[0];
         }
     }
 }
+
+
